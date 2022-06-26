08658d458a989246e519eb709f5c0a008b0c0d49
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-06-26 03:42:38 +0200

    add output units for Bridge Guard

---
+++ _work/Data/Scripts/content/MOD/subtitles.d
@@ -0,0 +1,10 @@
+/*
+ *	Subtitles
+ *	 - this function 'SVM_Subtitles' does not have to be called, what we need is only compilation in order to get outputs units below available for our cutscenes
+ */
+func void SVM_Subtitles () {
+	//Bridge Guard
+	AI_Output (null, null, "DIA_BridgeGuard_275_Intro_06_00"); //Fresh meat for our mine, it was about the time!
+	AI_Output (null, null, "DIA_BridgeGuard_275_Intro_06_01"); //I am sure there is a pickaxe lying somewhere around here, haha!
+
+};
