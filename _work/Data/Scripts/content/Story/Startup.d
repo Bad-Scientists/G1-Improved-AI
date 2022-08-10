097d05cfb1ace49f00e9c7a7678e69781d1ae0ad
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-08-11 00:27:17 +0200

    enable custom perceptions from AFSP
    Custom perception functions will allow us to run new AI logic while 'default' AI is busy for example by walking.

--- _work/Data/Scripts/content/Story/Startup.d
+++ _work/Data/Scripts/content/Story/Startup.d
@@ -4,6 +4,12 @@ func void Init_Global () {
 
 	//LeGo
 	LeGo_Init (LeGo_HookEngine | LeGo_View | LeGo_AI_Function | LeGo_Interface | LeGo_PermMem | LeGo_ConsoleCommands | LeGo_GameState | LeGo_Cursor | LeGo_FrameFunctions | LeGo_EventHandler | LeGo_Saves | LeGo_Bars | LeGo_PrintS);
+
+	//--> AFSP
+	//Additional perceptions
+	G12_AddPerceptions_Init ();
+
+	//<--
 };
 
 func void STARTUP_SUB_PSICAMP()
