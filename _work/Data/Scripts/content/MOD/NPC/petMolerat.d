d325882f102f5595ed7d66d83e00d259b75ddaa7
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-08-26 23:40:26 +0200

    remove default inventory

--- _work/Data/Scripts/content/MOD/NPC/petMolerat.d
+++ _work/Data/Scripts/content/MOD/NPC/petMolerat.d
@@ -3,7 +3,9 @@ instance PetMolerat(Mst_Default_Molerat)
 	Set_Molerat_Visuals();
 	Npc_SetToFistMode(self);
 
-	CreateInvItems(self, ItFoMuttonRaw, 2);
+	//By default our Npc will not have anything in its inventory.
+	//It's because it will be picking up items ... and 'throwing them up' onto player :)
+	//CreateInvItems(self, ItFoMuttonRaw, 2);
 
 	//Same senses as summoned creatures - will ignore enemies unless it sees them
 	senses = SENSE_HEAR | SENSE_SEE;
