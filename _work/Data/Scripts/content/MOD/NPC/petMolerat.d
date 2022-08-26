ad94a6a871eaf6720d49dacaafbee6547dbe67d7
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2022-08-26 23:39:29 +0200

    update Npc instance
    Adding same senses as summoned creatures, min distance to player and partymember flag.

--- _work/Data/Scripts/content/MOD/NPC/petMolerat.d
+++ _work/Data/Scripts/content/MOD/NPC/petMolerat.d
@@ -4,5 +4,14 @@ instance PetMolerat(Mst_Default_Molerat)
 	Npc_SetToFistMode(self);
 
 	CreateInvItems(self, ItFoMuttonRaw, 2);
+
+	//Same senses as summoned creatures - will ignore enemies unless it sees them
+	senses = SENSE_HEAR | SENSE_SEE;
+
+	//Distance to player and partymember
+	self.aivar[AIV_MM_DistToMaster] = 500;
+	self.aivar[AIV_MM_PARTYMEMBER] = TRUE;
+
+	start_aistate = ZS_MM_PetMolerat;
 };
 
