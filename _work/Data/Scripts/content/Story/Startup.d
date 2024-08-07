42e28dc2c13b78cbfd99ce03e17d2feccc25fb72
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2024-08-07 23:41:23 +0200

    initialize Ikarus
    G1 does not have Init_Global() function. We have to add it to every Init_[world_name] function.

--- _work/Data/Scripts/content/Story/Startup.d
+++ _work/Data/Scripts/content/Story/Startup.d
@@ -1,3 +1,8 @@
+func void Init_Global () {
+	//Ikarus
+	MEM_InitAll();
+};
+
 func void STARTUP_SUB_PSICAMP()
 {
 	// -----------OLD MINE-----------------------------------------
@@ -2216,6 +2221,8 @@ func void INIT_SURFACE()
 // -------- eigentliche Startup des Spiels --------
 func void INIT_WORLD()
 {
+	Init_Global();
+
 	INIT_Sub_Newcamp();
 	INIT_Sub_Oldcamp();
 	INIT_Sub_Psicamp();
@@ -2301,6 +2308,8 @@ func void STARTUP_ORCGRAVEYARD()
 
 func void INIT_ORCGRAVEYARD()
 {
+	Init_Global();
+
 	Wld_SetMobRoutine(00, 00, "FIREPLACE", 1);
 
 	// -------- Attitüden initialisieren --------
@@ -2434,6 +2443,8 @@ func void STARTUP_ORCTEMPEL()
 
 func void INIT_ORCTEMPEL()
 {
+	Init_Global();
+
 	// -------- Objekt-TAs --------
 	Wld_SetMobRoutine(00, 00, "FIREPLACE", 1);
 
@@ -2561,6 +2572,8 @@ func void STARTUP_OLDMINE()
 
 func void INIT_OLDMINE()
 {
+	Init_Global();
+
 	Wld_SetMobRoutine(00, 00, "FIREPLACE", 1);
 
 	// -------- Attitüden initialisieren --------
@@ -2633,6 +2646,8 @@ func void STARTUP_FREEMINE()
 
 func void INIT_FREEMINE()
 {
+	Init_Global();
+
 	// -------------------Fackeln-----------------------------
 	Wld_SetMobRoutine(00, 00, "FIREPLACE", 1);
 
